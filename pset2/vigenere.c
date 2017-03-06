72ec946c076a16251b9578ed9c4ba199bb77af73
Natalia Ostiak <natalya.sosnovsckaya@gmail.com>
2017-03-06 14:07:27 +0200

    Create vigenere.c

---
+++ pset2/vigenere.c
@@ -0,0 +1,65 @@
+/**
+ * vigenere.c
+ * 
+ * Natalya Ostiak
+ * Technology Nation
+ * 
+ * Encrypts messages using Vigenère’s cipher.
+ */
+ 
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+// prototype 
+void PrintLetters(char a, char b, int c, int *d);
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage:./vigenere key\n");
+        return 1;
+    }
+    
+    string k = argv[1];
+    for (int j = 0, n = strlen(k); j < n; j++)
+    
+        if (! isalpha(k[j]))
+        {
+            printf("Key is a set of characters from A to Z or from a to z\n");
+            return 1; 
+        }
+    
+    printf("plaintext:  ");    
+    string s = GetString();
+    printf("ciphertext: ");
+    
+    for (int j = 0, n = strlen(k); j < n; j++)
+        k[j] = toupper(k[j]);
+    int step;    
+    int flag = 0;    
+    for (int i =  0, n = strlen(s); i < n; i++)       
+    {
+        if (isalpha(s[i]))
+            {
+                if (isupper(s[i]))
+                    step = 65;
+                else 
+                    step = 97;  
+                PrintLetters(s[i], k[flag%strlen(k)], step, &flag);
+            }
+        
+        else
+            printf("%c", s[i]);
+    }
+    printf("\n");
+    return 0;
+}  
+
+void PrintLetters(char sf, char kf, int stepf, int *flagf)
+{
+    printf("%c", (sf - stepf + kf-65) % 26 + stepf);
+    (*flagf)++;
+}    
