59597f74a877c211e202b581634471ece66d90ca
Natalia Ostiak <natalya.sosnovsckaya@gmail.com>
2017-04-27 02:26:04 +0300

    Update credit.c

--- pset1/credit.c
+++ pset1/credit.c
@@ -39,53 +39,53 @@ int main(void)
     }
     while (cc_num_D > 0);
     
-    // Define an array
-    int cc_num_A[d_num];
-    
     if (d_num!=15 && d_num!=16 && d_num!=13)
     {
         printf("INVALID\n");
-         return 0;
-    }     
+        return 0;
+    }  
     
-// Build the reverse array
-for (int i = 0; i < d_num; i++)
-{
-    cc_num_A[i] = cc_num % 10;
-    cc_num = cc_num / 10;
-}
+    // Define an array
+    int cc_num_A[d_num];    
+    
+    // Build the reverse array
+    for (int i = 0; i < d_num; i++)
+    {
+        cc_num_A[i] = cc_num % 10;
+        cc_num = cc_num / 10;
+    }
    
-// Luhn’s algorithm
-for (int i = 0; i < d_num; i = i + 2)
-    sum_odd = sum_odd + cc_num_A[i];
+    // Luhn’s algorithm
+    for (int i = 0; i < d_num; i = i + 2)
+        sum_odd = sum_odd + cc_num_A[i];
     
-for (int i = 1; i < d_num; i = i + 2)
-{
-    int even = cc_num_A[i] * 2;
+    for (int i = 1; i < d_num; i = i + 2)
+    {
+        int even = cc_num_A[i] * 2;
             
-    if (even > 9)
-        sum_even = sum_even + even - 9;
-    else
-        sum_even = sum_even + even;
-}
+        if (even > 9)
+            sum_even = sum_even + even - 9;
+        else
+            sum_even = sum_even + even;
+    }   
     
-sumCheck = (sum_odd + sum_even) % 10;
+    sumCheck = (sum_odd + sum_even) % 10;
     
-if (sumCheck != 0)
-    printf("INVALID\n");
+    if (sumCheck != 0)
+        printf("INVALID\n");
         
-else if (d_num==15 && cc_num_A[d_num-1]==3 && (cc_num_A[d_num-2]==4 || cc_num_A[d_num-2]==7))
-        printf("AMEX\n");
+    else if (d_num==15 && cc_num_A[d_num-1]==3 && (cc_num_A[d_num-2]==4 || cc_num_A[d_num-2]==7))
+            printf("AMEX\n");
         
-    else if (d_num==16 && cc_num_A[d_num-1]==5 && (cc_num_A[d_num-2]==1 || cc_num_A[d_num-2]==2 
-                        || cc_num_A[d_num-2]==3 || cc_num_A[d_num-2]==4 || cc_num_A[d_num-2]==5))
-            printf("MASTERCARD\n");
+        else if (d_num==16 && cc_num_A[d_num-1]==5 && (cc_num_A[d_num-2]==1 || cc_num_A[d_num-2]==2 
+                            || cc_num_A[d_num-2]==3 || cc_num_A[d_num-2]==4 || cc_num_A[d_num-2]==5))
+                printf("MASTERCARD\n");
             
-        else if ((d_num==13 || d_num==16) && cc_num_A[d_num-1]==4)
-                printf("VISA\n");
+            else if ((d_num==13 || d_num==16) && cc_num_A[d_num-1]==4)
+                    printf("VISA\n");
         
-             else
-                printf("INVALID\n");
+                 else
+                     printf("INVALID\n");
                     
-return 0;
+    return 0;
 }
